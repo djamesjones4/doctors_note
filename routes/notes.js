78e4443c9d04f25635f6595b80686f143029bd9c
Derek Jones <djamesjones4@gmail.com>
2017-06-29 12:45:54 -0600

    notes route workng on gettng by client_id and pract_id

--- routes/notes.js
+++ routes/notes.js
@@ -6,18 +6,42 @@ const knex = require('../knex')
 const jwt = require('jsonwebtoken')
 
 // GET notes for practitioner or client
+// GET notes where client_id && practitioner_id
+router.get('/', function(req, res, next) {
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    let practitioner_id = req.body.practitioner_id
+    let client_id = req.body.client
+    if (payload) {
+      user_name = payload.username
+      getClientNotes(user_name)
+    } else if (err) {
+      res.send('please log in')
+    }
+
+    function getClientNotes(username) {
+      // get note by id
+      knex('notes')
+        .where('id', noteId)
+        .then((data) => {
+          console.log('clients data: ', data)
+          res.send(data)
+        })
+    }
+  })
+})
+// GET notes by id
 router.get('/:id', function(req, res, next) {
   jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
     let user_name
     let noteId = req.params.id
     if (payload) {
       user_name = payload.username
-      getClientInfo(user_name)
+      getSpecificNote(user_name)
     } else if (err) {
       res.send('please log in')
     }
 
-    function getClientInfo(username) {
+    function getSpecificNote(username) {
       // get note by id
       knex('notes')
         .where('id', noteId)
