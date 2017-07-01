3261ae491f2eebfef5f51937a304004d90eb7a43
Derek Jones <djamesjones4@gmail.com>
2017-07-01 16:50:08 -0600

    seeded pract_type

--- seeds/2_practitioners.js
+++ seeds/2_practitioners.js
@@ -12,6 +12,7 @@ exports.seed = function(knex, Promise) {
             username: 'drjones',
             hashed_password: '$2a$12$dfoHuqRpfqkIZMcgt/Hw0eJnMFbT8/7fljzzy1B5sA7n7SILKxxra',
             email: 'jonesder@msu.edu',
+            practitioner_type: 'physician',
             isclient: false,
             ispractitioner: true,
             isadmin: false
