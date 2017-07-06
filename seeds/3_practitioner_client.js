fcd0713087c358b5260085f8c911ef8e264255ef
Derek Jones <djamesjones4@gmail.com>
2017-07-06 04:12:09 -0600

    updated pract_client table seed data

--- seeds/3_practitioner_client.js
+++ seeds/3_practitioner_client.js
@@ -17,6 +17,18 @@ exports.seed = function(knex, Promise) {
             id: 3,
             client_id: 1,
             practitioner_id: 2
+          }, {
+            id: 4,
+            client_id: 1,
+            practitioner_id: 3
+          }, {
+            id: 5,
+            client_id: 1,
+            practitioner_id: 4
+          },{
+            id: 6,
+            client_id: 1,
+            practitioner_id: 5
           }
         ])
       ])
